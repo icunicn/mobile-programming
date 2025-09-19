9ae68792b85eb7d80ccbb5e24b54e8d19bab4bc1
icunicn <mkharisi2004@gmail.com>
2025-09-19 23:09:50 +0700

    Initial Commit Praktikum Modul 5

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
-# Praktikum Modul 1 Row and Column
+# Praktikum Modul 5 Widget SizedBox, Spacer, dan Card
 
 A new Flutter project.
-
 
 ## Project Output (Screenshots)
 
 Screenshots are stored in assets/screenshots.
 
-- Output 1:  
+- Output 1:
 
   <img src="assets/screenshots/1.jpg" alt="Single thumbs up icon" width="200" height="auto" />
 
@@ -15,4 +14,30 @@ Screenshots are stored in assets/screenshots.
 
   <img src="assets/screenshots/2.jpg" alt="Three thumbs icons in Row and Column layout" width="200" height="auto" />
 
+- Output 3:
 
+  <img src="assets/screenshots/3.jpg" alt="Three thumbs icons in Row and Column layout" width="200" height="auto" />
+
+- Output 4:
+
+  <img src="assets/screenshots/4.jpg" alt="Three thumbs icons in Row and Column layout" width="200" height="auto" />
+
+- Output 5:
+
+  <img src="assets/screenshots/5.jpg" alt="Three thumbs icons in Row and Column layout" width="200" height="auto" />
+
+- Output 6:
+
+  <img src="assets/screenshots/6.jpg" alt="Three thumbs icons in Row and Column layout" width="200" height="auto" />
+
+- Output 7:
+
+  <img src="assets/screenshots/7.jpg" alt="Three thumbs icons in Row and Column layout" width="200" height="auto" />
+
+- Output 8:
+
+  <img src="assets/screenshots/8.jpg" alt="Three thumbs icons in Row and Column layout" width="200" height="auto" />
+
+- Output 9:
+
+  <img src="assets/screenshots/9.jpg" alt="Three thumbs icons in Row and Column layout" width="200" height="auto" />
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -12,70 +14,63 @@ class MyApp extends StatelessWidget {
       title: 'Contoh Sizedbox',
       theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(title: const Text('Sedang Memutar'), centerTitle: true),
-
+        appBar: AppBar(title: const Text('Sedang memutar'), centerTitle: true),
         body: Center(
-          child: Card(
-            margin: const EdgeInsets.all(8),
-            elevation: 4,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(12),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: const Icon(
-                      Icons.album,
-                      size: 100,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              'Dear God',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(width: 8),
-                            Text(
-                              'by Avenged Sevenfold',
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.grey,
-                              ),
-                            ),
-                            Spacer(),
-                            IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.favorite_border,
-                                color: Colors.pinkAccent,
-                              ),
-                            ),
-                          ],
-                        ),
+          child: Container(
+            child: Card(
+              elevation: 4,
+              color: Colors.grey,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 96,
+                      height: 96,
+                      child: const Center(
+                        child: Icon(Icons.album, size: 100, color: Colors.grey),
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    const SizedBox(height: 12),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: const [
+                              Text(
+                                'Dear God',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                'Avenged Sevenfold',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.favorite_border),
+                          color: Colors.pinkAccent,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
