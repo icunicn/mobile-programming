bd497a63f81b1373129ca275936421ab4f03ebd4
icunicn <mkharisi2004@gmail.com>
2025-09-19 08:00:16 +0700

    Tugas modul 5

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Contoh Sizedbox',
+      theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Praktikum Flutter'
-        )),
-        body: const Center(
-          child: Text(
-            'Muhamad Kharis Ihsan',
-            style: TextStyle(fontSize: 24),
-          )
+        appBar: AppBar(title: const Text('Sedang Memutar'), centerTitle: true),
+
+        body: Center(
+          child: Card(
+            margin: const EdgeInsets.all(8),
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Padding(
+              padding: EdgeInsets.all(12),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    child: const Icon(
+                      Icons.album,
+                      size: 100,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Dear God',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Text(
+                              'by Avenged Sevenfold',
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            Spacer(),
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.favorite_border,
+                                color: Colors.pinkAccent,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
-      )
+      ),
     );
   }
 }
-
